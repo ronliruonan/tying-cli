293d571e72d90db53190095dd58f3683206a6e21
Ronan <liruonan@hotmail.com>
2020-12-13 16:17:46 +0800

    update: tying add confirg

--- bin/tying-add.js
+++ bin/tying-add.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 // 引入问答交互模块
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 // 引入文件操作模块
-const fs = require('fs')
+const fs = require('fs');
 // 引入路径操作
-const path = require('path')
+const path = require('path');
 // tying-template.json路径
-const tpath = path.resolve(__dirname, '../tying-template.json')
+const tpath = path.resolve(__dirname, '../tying-template.json');
 // 获取模板列表内容
-let tyingTlps = require(tpath) // 默认 []
+let tyingTlps = require(tpath); // 默认 []
 
 
 
@@ -22,18 +22,25 @@ let questions = [
     },
     {
         type: 'input',
-        name: 'tpll-git',
-        message: '请输入git url'
+        name: 'tpl-url',
+        message: '请输入 url'
     }
-]
+];
 
 // 交互式回答 add 信息处理
 inquirer
     .prompt(questions).then(answers => {
-        console.log(answers)
+        console.log(answers);
         // 获取问答内容
-        let tplName = answers['tpl-name']
-        let tplGit = answers['tpl-git']
+        let tplName = answers['tpl-name'];
+        let tplUrl = answers['tpl-url'];
 
         // 更新到tying-template.json中
-    })
+        tyingTlps.push({
+            name: tplName,
+            url: tplUrl
+        });
+
+        // 更新 tpl文件内容
+        fs.writeFileSync(tpath, JSON.stringify(tyingTlps));
+    });
